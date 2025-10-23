398d180eef7930e913c8e5547b94e8db10dd465a
dmitrii.kalenitskii <dogee4803@gmail.com>
2025-10-23 23:59:38 +0300

    Setting up VuePrime

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+import Aura from '@primeuix/themes/aura';
+import Button from "primevue/button"
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.component('Button', Button);
+
+app.mount('#app');
